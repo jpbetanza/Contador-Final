ff6475438257ee2b15936326ba416b316bfc5239
jpbetanza <jpbetanza99@gmail.com>
2023-08-08 00:12:18 -0300

    Ajustes

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ var audio = new Audio("./src/StereoLove.mp3")
 audio.loop = true
 
 
+//Cronometro
 var cronometro = setInterval(function() {
     var now = new Date().getTime();
     var timeleft = countDownDate - now;
@@ -11,17 +12,11 @@ var cronometro = setInterval(function() {
     var hours = Math.floor((timeleft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     var minutes = Math.floor((timeleft % (1000 * 60 * 60)) / (1000 * 60));
     var seconds = Math.floor((timeleft % (1000 * 60)) / 1000);
-
-    document.getElementById("id1").innerHTML = days + ":" + hours + ":" + minutes + ":" + seconds;
-    if(timeleft==0){
+    if(timeleft>0){
+      document.getElementById("id1").innerHTML = days + ":" + hours + ":" + minutes + ":" + seconds;
+    }
+    else{
+      document.getElementById("id1").innerHTML = "É isto"
       audio.play()
     }
   }, 1000)
-var bgColor = setInterval(function() {
-  coming
-  start
-  middle
-  end
-
-  document.getElementById('bod').classList.value = "test"
-},50)
